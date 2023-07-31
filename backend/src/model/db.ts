3149b7df98a493491c6f1a65ec04e1ed4ac574af
Dmitry Kulakov <atomeistee@gmail.com>
2023-07-31 07:52:01 +0300

    fix: in previous node versions node doesn`t has global scope fetch
    https://github.com/drizzle-team/drizzle-orm/issues/242

--- backend/src/model/db.ts
+++ backend/src/model/db.ts
@@ -2,6 +2,7 @@ import { Kysely } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
 import { ColumnType } from "kysely";
 import dotenv from "dotenv";
+import { fetch } from "undici";
 import { DB, Champions } from "~/types/db.js";
 dotenv.config();
 if (!process.env.DATABASE_HOST) throw new Error("DATABASE_HOST not set");
@@ -10,11 +11,11 @@ if (!process.env.DATABASE_USERNAME)
 if (!process.env.DATABASE_PASSWORD)
   throw new Error("DATABASE_PASSWORD not set");
 
-
 export const db = new Kysely<DB>({
   dialect: new PlanetScaleDialect({
     host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
+    fetch,
   }),
 });

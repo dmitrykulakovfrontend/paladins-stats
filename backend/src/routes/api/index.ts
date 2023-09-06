5fe020fe4f68cc71663986d3735d509ab3c30873
Dmitry Kulakov <atomeistee@gmail.com>
2023-09-06 13:30:41 +0300

    feat: roles data route

--- backend/src/routes/api/index.ts
+++ backend/src/routes/api/index.ts
@@ -102,7 +102,7 @@ router.get(
 router.get(
   "/champions/:id",
   catchErrors(async (req, res) => {
-    const globalStatsResponse = await db
+    const globalStats = await db
       .selectFrom("champions as c")
       .innerJoin("daily_stats as ds", "c.id", "ds.champion_id")
       .innerJoin(
@@ -173,27 +173,70 @@ router.get(
       ])
       .execute();
     res.status(200).json({
-      globalStats: globalStatsResponse,
+      globalStats: globalStats,
       weeklyStats,
     });
   })
 );
 
-// const { Query } = require('kysely');
+router.get(
+  "/roles",
+  catchErrors(async (_req, res) => {
+    const rolesData = await db
+      .selectFrom("champions as c")
+      .innerJoin("daily_stats as ds", "c.id", "ds.champion_id")
+      .innerJoin(
+        "global_daily_stats as gds",
+        "ds.the_day_total_matches_id",
+        "gds.id"
+      )
+      .select([
+        "c.role",
+        sql`ROUND(AVG(ds.wins / (ds.wins + ds.loses) * 100), 2)`.as("winrate"),
+        sql`ROUND(SUM(ds.wins + ds.loses) / (select sum(gds2.matches) from global_daily_stats as gds2 where gds2.date >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)) * 100, 2)`.as(
+          "pickrate"
+        ),
+        sql`ROUND(AVG((ds.kills + (ds.assists / 2)) / ds.deaths), 2)`.as("KDA"),
+        sql`ROUND(AVG(ds.damage / (ds.match_duration / 60) * 10), 2)`.as(
+          "damage_10_min"
+        ),
+        sql`ROUND(AVG(ds.deaths / (ds.match_duration / 60) * 10), 2)`.as(
+          "deaths_10_min"
+        ),
+        sql`ROUND(AVG(ds.assists / (ds.match_duration / 60) * 10), 2)`.as(
+          "assists_10_min"
+        ),
+        sql`ROUND(AVG(ds.kills / (ds.match_duration / 60) * 10), 2)`.as(
+          "kills_10_min"
+        ),
+        sql`ROUND(AVG(ds.solo_kills / (ds.match_duration / 60) * 10), 2)`.as(
+          "solo_kills_10_min"
+        ),
+        sql`ROUND(AVG(ds.self_healing / (ds.match_duration / 60) * 10), 2)`.as(
+          "self_healing_10_min"
+        ),
+        sql`ROUND(AVG(ds.gold_per_minute / (ds.match_duration / 60) * 10), 2)`.as(
+          "gold_per_minute_10_min"
+        ),
+        sql`ROUND(AVG(ds.healing / (ds.match_duration / 60) * 10), 2)`.as(
+          "healing_10_min"
+        ),
+        sql`ROUND(AVG(ds.shielding / (ds.match_duration / 60) * 10), 2)`.as(
+          "shielding_10_min"
+        ),
+        sql`ROUND(AVG(ds.objective_time / (ds.match_duration / 60) * 10), 2)`.as(
+          "objective_time_10_min"
+        ),
+      ])
+      .where(sql`ds.date >= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)`)
+      .groupBy("c.role")
+      .orderBy("pickrate", "desc")
+      .execute();
 
-// const query = new Query()
-//   .select([
-//     'pet_id',
-//     'DATE_TRUNC("week", walk_date) AS week_start_date',
-//     'SUM(distance_walked_km) AS total_distance_walked',
-//     'COUNT(walk_date) AS days_walked',
-//     'AVG(distance_walked_km) AS weekly_average_distance'
-//   ])
-//   .from('pet_walks')
-//   .groupBy(['pet_id', 'DATE_TRUNC("week", walk_date)']);
+    res.status(200).json({
+      rolesData,
+    });
+  })
+);
 
-// console.log(query.toString());
-
-//   `
-//   `
 export default router;

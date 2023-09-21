8b3d1039f8f07d3548e63d3e0da5389e32a12f91
Dmitry Kulakov <atomeistee@gmail.com>
2023-09-21 16:58:46 +0300

    fix hosting error

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,6 +1,5 @@
 import chalk from "chalk";
 import morgan from "morgan";
-import { getReasonPhrase } from "http-status-codes";
 import axios from "axios";
 
 type statuses = "Error" | "Warning" | "Info";
@@ -33,9 +32,7 @@ export const logger = morgan(function (tokens, req, res) {
     chalk.hex("#f94144").bold("🍄  Logs --> "), // red
     chalk.hex("#90be6d").bold(tokens.method(req, res)), // green
     chalk.hex("#f9c74f").bold(tokens.status(req, res) + " ="), // yellow
-    chalk
-      .hex("#90be6d")
-      .bold(getReasonPhrase(tokens.status(req, res) as string)), // green
+    chalk.hex("#90be6d").bold(tokens.status(req, res)), // green
     chalk.hex("#43aa8b").bold(tokens.url(req, res)), // teal
     chalk.hex("#577590").bold(tokens["response-time"](req, res) + " ms"), // blue-gray
     chalk.hex("#f94144").bold("@ " + tokens.date(req, res)), // red
